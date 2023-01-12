d0323e2fedb47bc6848bd4655fc73d39edc0803a
James Montaño <jamesmontano099@gmail.com>
2023-01-12 14:07:02 -0500

    Updates

---
+++ precache-manifest.9863ee22a1a7ba0b89b5ddb8b8d1ddb4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "8d27747b632e90d7e759",
+    "url": "/1-introduccion/static/css/main.06a80570.chunk.css"
+  },
+  {
+    "revision": "8d27747b632e90d7e759",
+    "url": "/1-introduccion/static/js/main.a94732d3.chunk.js"
+  },
+  {
+    "revision": "4f229a1cc1c0669d54b9",
+    "url": "/1-introduccion/static/js/runtime~main.97c04111.js"
+  },
+  {
+    "revision": "27e658d0dc6bc3f0c25b",
+    "url": "/1-introduccion/static/js/2.b1e17e5c.chunk.js"
+  },
+  {
+    "revision": "8dc45afd6274dbe41cb0d32678af9eb9",
+    "url": "/1-introduccion/static/media/check.8dc45afd.svg"
+  },
+  {
+    "revision": "76c25a2bf88e4a90139bb77633cd8e32",
+    "url": "/1-introduccion/static/media/delete.76c25a2b.svg"
+  },
+  {
+    "revision": "46a9ed05190edf8f5d9471fb5fb80a0c",
+    "url": "/1-introduccion/index.html"
+  }
+];
